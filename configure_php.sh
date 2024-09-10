840b805cf2b06ce658f3be948ff2a7b96493d0c5
Tomas Roun <tomas.roun@cern.ch>
2024-09-10 15:30:00 +0200

    Update php config

--- configure_php.sh
+++ configure_php.sh
@@ -4,3 +4,5 @@ ini_path=$(php -i | grep /.+/php.ini -oE)
 
 # Allow <? ?> tags in PHP
 sed -i "s/short_open_tag = .*/short_open_tag = On/" $ini_path
+
+sed -i "s/;error_log = php_errors.log/error_log = STDERR/" $ini_path
